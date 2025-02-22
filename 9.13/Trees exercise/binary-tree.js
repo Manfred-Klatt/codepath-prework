3dbd632629de89bc10390a1fbb7dd10922f0cedb
Manfred-Klatt <manfredjklatt@gmail.com>
2025-02-22 12:15:39 -0500

    Completed Trees exercise

---
+++ 9.13/Trees exercise/binary-tree.js
@@ -0,0 +1,47 @@
+class BinaryTreeNode {
+  constructor(val, left = null, right = null) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class BinaryTree {
+  constructor(root = null) {
+    this.root = root;
+  }
+
+  minDepth() {
+
+  }
+
+  maxDepth() {
+
+  }
+ 
+  maxSum() {
+
+  }
+ 
+  nextLarger(lowerBound) {
+
+  }
+
+  areCousins(node1, node2) {
+
+  }
+
+  static serialize() {
+
+  }
+
+  static deserialize() {
+
+  }
+
+  lowestCommonAncestor(node1, node2) {
+    
+  }
+}
+
+module.exports = { BinaryTree, BinaryTreeNode };
---
+++ 9.13/Trees exercise/tree.js
@@ -0,0 +1,26 @@
+class TreeNode {
+  constructor(val, children = []) {
+    this.val = val;
+    this.children = children;
+  }
+}
+
+class Tree {
+  constructor(root = null) {
+    this.root = root;
+  }
+
+  sumValues() {
+    
+  }
+
+  countEvens() {
+
+  }
+
+  numGreater(lowerBound) {
+
+  }
+}
+
+module.exports = { Tree, TreeNode };
